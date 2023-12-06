3c62bafd8e6e60d76cf8d066c0039d9ec24b1c80
NicolasDISEP <nicolas.nd40@gmail.com>
2023-12-06 12:19:29 +0100

    sort Blade of the Ruined King fini

--- branch nicolas/test.py
+++ branch nicolas/test.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 class ChampionScraper:
     def __init__(self, url):
@@ -11,7 +12,8 @@ class ChampionScraper:
         return BeautifulSoup(response.text, "html.parser")
 
     def find_element_by_label(self, label, class_name):
-        balise_h3 = self.soup.find('h3', class_='pi-data-label {}'.format(class_name), string=label)
+        selector = f'h3.pi-data-label.{class_name}:contains("{label}")'
+        balise_h3 = self.soup.select_one(selector)
 
         if balise_h3:
             balise_suivante = balise_h3.find_next_sibling()
@@ -21,54 +23,24 @@ class ChampionScraper:
 
         return None
 
-    def get_cooldown_value(self):
-        return self.find_element_by_label('COOLDOWN:', 'pi-secondary-font')
-
     def get_bonus_physical_damage(self):
-        balises_dl = self.soup.find_all('dl', class_='skill-tabs')
-    
-        for balise_dl in balises_dl:
+        for balise_dl in self.soup.find_all('dl', class_='skill-tabs'):
             dt = balise_dl.find('dt')
             dd = balise_dl.find('dd')
-            
+
             if dt and dd:
                 texte = dd.find('span', style="color: #1F995C; white-space:normal")
-        
-        return texte
-
-    def get_spell_range(self):
-        ability_header_div = self.soup.find('div', {'class': 'champion-ability__header'})
-
-        if ability_header_div:
-            range_div = ability_header_div.find_next('div', {'data-source': 'range'})
-
-            if range_div:
-                range_value_tag = range_div.find('span', {'class': 'basic-tooltip'})
-
-                if range_value_tag:
-                    return range_value_tag.text.strip()
-
-        return None
-
-    def get_width_value(self):
-        width_div = self.soup.find('div', {'data-source': 'width'})
-
-        if width_div:
-            width_value_tag = width_div.find('span', {'class': 'basic-tooltip'})
-
-            if width_value_tag:
-                return width_value_tag.text.strip()
+                if texte:
+                    return texte.get_text(strip=True)
 
         return None
 
     def Bonus_Damage(self):
         stats_div = self.soup.find('div', {'style': 'padding: 0 7px'})
-
         if stats_div:
-            stats_dl_list = stats_div.find_all('dl', class_='skill-tabs')
             stats = {}
 
-            for stats_dl in stats_dl_list:
+            for stats_dl in stats_div.find_all('dl', class_='skill-tabs'):
                 dt = stats_dl.find('dt')
                 dd = stats_dl.find('dd')
 
@@ -82,42 +54,67 @@ class ChampionScraper:
         return None
 
     def format_to_list(self, chaine):
-        nombres = []
-        nombre_temporaire = ""
-        est_dans_nombre = False
+        return [float(nombre) for nombre in re.findall(r'\d+\.\d+|\d+', chaine)]
+    
+    def get_divs_above_img(self, img_alt_text):
+        img_tag = self.soup.find('img', alt=img_alt_text)
 
-        for caractere in chaine:
-            if caractere.isdigit() or caractere == '.':
-                nombre_temporaire += caractere
-                est_dans_nombre = True
-            elif est_dans_nombre:
-                nombres.append(float(nombre_temporaire))
-                nombre_temporaire = ""
-                est_dans_nombre = False
+        if img_tag:
+            parent_div = img_tag.find_parent('div')
+            divs_above = parent_div.find_all_previous('div', limit=2)
 
-        if est_dans_nombre:
-            nombres.append(float(nombre_temporaire))
+            return divs_above
 
-        return nombres
+    def get_orange_and_blue_text(self, divs_above_img):
+        orange_and_blue_text = []
+
+        for div in divs_above_img:
+            orange_text = div.find_all('span', style="color:orange; white-space:normal")
+            blue_text = div.find_all('span', style="color: #7A6DFF; white-space:normal")
+
+            for text in orange_text:
+                orange_and_blue_text.append(text.get_text(strip=True))
+
+            for text in blue_text:
+                orange_and_blue_text.append(text.get_text(strip=True))
+
+        return orange_and_blue_text
+
+    def find_physical_damage_table(self):
+        physical_damage_span = self.soup.find('span', {'class': 'template_lc'}, string='Physical Damage:')
+        if physical_damage_span:
+            table = physical_damage_span.find_previous('table')
+            dd_elements = table.find_all('dd', class_='')
+            dd_texts = [dd.get_text(strip=True) for dd in dd_elements]
+
+        return dd_texts
+
+    def find_physical_damage_span(self):
+        return self.soup.find('span', {'class': 'template_lc'}, string='Physical Damage:')
 
     def main(self):
-        cooldown_value = self.get_cooldown_value()
+        cooldown_value = self.find_element_by_label('COOLDOWN:', 'pi-secondary-font')
         cooldown_value = self.format_to_list(cooldown_value)
 
         print("Cooldown\n")
         print(cooldown_value)
         print("\n")
-        print("Spell Range")
-        print(self.get_spell_range())
-        print("\n")
-        print("Width")
-        print(self.get_width_value())
-        print("\n")
         print("Bonus Damage Max/Min")
         print(self.Bonus_Damage())
         print("\n")
         print("Heartbreaker BONUS PHYSICAL DAMAGE")
-        print(self.get_bonus_physical_damage()) 
+        print(self.get_bonus_physical_damage())
+        print("\n")
+        print("bonus damage 2")
+        img_alt_text = 'Blade of the Ruined King'
+        divs_above_img = self.get_divs_above_img(img_alt_text)
+        if divs_above_img:
+            orange_and_blue_text = self.get_orange_and_blue_text(divs_above_img)
+            print(orange_and_blue_text)
+        physical_damage_table = self.find_physical_damage_table()
+        if physical_damage_table:
+            print("\nPhysical Damage Table:")
+            print(physical_damage_table)
 
 if __name__ == "__main__":
     scraper = ChampionScraper("https://leagueoflegends.fandom.com/wiki/Viego/LoL")
