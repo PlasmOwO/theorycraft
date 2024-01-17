e882177fc57eb14103e1fe7e46df1985fbccf57a
PlasmOwO <thomas.plasmo@gmail.com>
2024-01-17 15:25:26 +0100

    New Item : Sundered Sky

---
+++ items/sundered_sky.py
@@ -0,0 +1,55 @@
+# # Web scraping for Sundered Sky
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = "https://leagueoflegends.fandom.com/wiki/Sundered_Sky"
+url
+
+response = requests.get(url)
+html = BeautifulSoup(response.text, "html.parser")
+
+# ## Base stats
+
+# +
+sundered = html.findAll("div",{"class" : "pi-data-value pi-font"})[:3]
+for idx,stats in enumerate(sundered):
+    sundered[idx] = re.search(r'(\d+)', stats.text).group(1)
+
+# take account of %
+sundered = list(map(int,sundered))
+print(sundered)
+# -
+
+# ## Auto empowered
+
+# +
+heal_span = html.find('span', {"data-tip" : "Heal"})
+
+empowered_auto = heal_span.find_previous(text=True)
+empowered_auto =  (int((re.findall(r'\b\d+\b|\b\d+\.\d+\b', empowered_auto)[0]))/100)
+# -
+
+empowered_auto
+
+# ## Item creation
+
+if __name__ == "__main__":
+    import sys
+    sys.path.append("..")
+    from class_item import Item
+else :
+    from class_item import Item
+import copy
+
+sundered_dict = {"ad" : sundered[0], "haste" : sundered[1], "hp" : sundered[2]}
+sundered_dict['pass_auto'] = empowered_auto
+
+
+Sundered_sky = Item("sundered_sky",sundered_dict)
+
+print(Sundered_sky)
+
+
