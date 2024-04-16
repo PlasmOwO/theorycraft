756ee2f6c165cfc05e99a3a88d2deeafe18d5e21
PlasmOwO <thomas.plasmo@gmail.com>
2024-04-16 21:29:34 +0200

    Auto stash before merge of "main" and "origin/main"

--- test.py
+++ test.py
@@ -35,9 +35,6 @@ import items.kraken_slayer as kraken_slayer
 import items.Titanic_hydra as titanic_hydra
 import items.black_cleaver as black_cleaver
 import items.sundered_sky as sundered_sky
-import items.triforce as triforce
-import items.terminus as terminus
-
 import champions.viego.scrapingViego as viego
 import class_array
 #import items.class_item
@@ -46,11 +43,8 @@ import class_array
 testBrk = brk.createBRK()
 testKraken = kraken_slayer.createKraken()
 testTitanic = titanic_hydra.createTitanic()
-print(testTitanic)
 testBC = black_cleaver.createBC()
 testSunderedSky = sundered_sky.createSunderedSky()
-testTriforce = triforce.createTriforce()
-testTerminus = terminus.createTerminus()
 
 # %%
 print(testBrk)
@@ -59,26 +53,17 @@ print(testBrk)
 #Create a champion object containing a .stats attribute which is a dataframe
 viegoBRK = viego.createViego()
 viegoBRK.addItem(testBrk)
-
 viegoKraken = viego.createViego()
 viegoKraken.addItem(testKraken)
-
+print(len(viegoKraken.stats))
 viegoTitanic = viego.createViego()
-viegoTitanic.addItem(testTitanic)
-print(viegoTitanic)
-
+viegoTitanic.addItem(viegoTitanic)
+viegoTitanic.stats
 #Not working
 viegoBC = viego.createViego()
 viegoBC.addItem(testBC)
 viegoSundered = viego.createViego()
 viegoSundered.addItem(testSunderedSky)
-
-viegoTriforce = viego.createViego()
-viegoTriforce.addItem(testTriforce)
-
-viegoTerminus = viego.createViego()
-viegoTerminus.addItem(testTerminus)
-
 
 
 # %% [markdown]
@@ -93,9 +78,6 @@ testArray.append(viegoBRK.stats.columns, viegoBRK.choseLvl(6))
 testArray.append(viegoKraken.stats.columns, viegoKraken.choseLvl(6))
 testArray.append(viegoBC.stats.columns, viegoBC.choseLvl(6))
 testArray.append(viegoSundered.stats.columns, viegoSundered.choseLvl(6))
-testArray.append(viegoTriforce.stats.columns, viegoTriforce.choseLvl(6))
-testArray.append(viegoTerminus.stats.columns, viegoTerminus.choseLvl(6))
-testArray.append(viegoTitanic.stats.columns, viegoTitanic.choseLvl(6))
 
 testArray.array['titanic_active_cleave'].replace(0,'0',inplace=True)
 testArray.array['titanic_passive_cleave'].replace(0,'0',inplace=True)
@@ -107,10 +89,7 @@ testArray.array
 
 
 # %%
-print(testArray.array['qDoubletap'])
-
-# %%
-testArray.array['titanic_active_cleave']
+print(testArray.array['qDoubletap'][2])
 
 # %%
 testArray.array.columns
@@ -220,3 +199,7 @@ for i in range(len(finalArrayNormalize)):
 # * passive * 50% PV
 
 # %%
+# Il reste, imlémenter ON HIT
+# Implémenter HP ennemy
+# Implémenter RESI ennemy
+# Implémenter tous les objets (notamment hydra qui bug)
